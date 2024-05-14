ff57851115f16dd075c6056164dd069d9cb12937
up202108794 <up202108794@edu.fe.up.pt>
2024-05-14 12:11:18 +0100

    started game logic. Added games on server.

--- assign2/src/hangman/src/main/java/fe/up/pt/Connection.java
+++ assign2/src/hangman/src/main/java/fe/up/pt/Connection.java
@@ -44,15 +44,29 @@ public class Connection {
       this.send("LGN:" + username + ":" + password);
    }
 
-    public void sendRegister(String username, String password) throws IOException {
-        this.send("REG:" + username + ":" + password);
-    }
+   public void sendRegister(String username, String password) throws IOException {
+      this.send("REG:" + username + ":" + password);
+   }
 
-    public void sendLogout(String sessionToken) throws IOException {
-        this.send("OUT:" + sessionToken);
-    }
+   public void sendLogout() throws IOException {
+      this.send("OUT:" + this.sessionToken);
+   }
+
+   public void sendGameStart() throws IOException {
+      this.send("GST:" + this.sessionToken);
+   }
+
+   public void sendGameLetter(String letter) throws IOException {
+      this.send("GSL:" + this.sessionToken + ":" + letter);
+   }
 
 
+
+   private void setTokenFromSuccessMessage(String message){
+      if (message.split(":")[0].trim().equals("SUC") && message.length() > 3){
+         this.sessionToken = message.split(":")[1].trim();
+      }
+   }
 
    public static void main(String[] args) throws IOException {
       Connection connection = new Connection(12345, "localhost");
@@ -61,38 +75,37 @@ public class Connection {
       Scanner scanner = new Scanner(System.in);
 
 
-        boolean active = true;
-        while(active) {
-           System.out.println("Login (L), Register (R) or Logout (O)?");
-           String action = scanner.nextLine();
-           while (!action.equals("L") && !action.equals("R") && !action.equals("O")) {
-              System.out.println("Invalid action. Login (L), Register (R) or Logout (O)?");
-              action = scanner.nextLine();
-           }
-           if (action.equals("L")) {
-              System.out.println("Enter username: ");
-              String username = scanner.nextLine();
-              System.out.println("Enter password: ");
-              String password = scanner.nextLine();
+      boolean active = true;
+      while (active) {
+         System.out.println("Login (L), Register (R) or Logout (O)?");
+         String action = scanner.nextLine();
+         while (!action.equals("L") && !action.equals("R") && !action.equals("O")) {
+            System.out.println("Invalid action. Login (L), Register (R) or Logout (O)?");
+            action = scanner.nextLine();
+         }
+         if (action.equals("L")) {
+            System.out.println("Enter username: ");
+            String username = scanner.nextLine();
+            System.out.println("Enter password: ");
+            String password = scanner.nextLine();
 
-              connection.sendLogin(username, password);
-           } else if (action.equals("R")) {
-              System.out.println("Enter username: ");
-              String username = scanner.nextLine();
-              System.out.println("Enter password: ");
-              String password = scanner.nextLine();
+            connection.sendLogin(username, password);
+         } else if (action.equals("R")) {
+            System.out.println("Enter username: ");
+            String username = scanner.nextLine();
+            System.out.println("Enter password: ");
+            String password = scanner.nextLine();
 
-              connection.sendRegister(username, password);
-           } else {
-              System.out.println("Enter session token: ");
-              String token = scanner.nextLine();
-              connection.sendLogout(token);
-              active = false;
-           }
+            connection.sendRegister(username, password);
+         } else {
+            connection.sendLogout();
+            active = false;
+         }
 
-              String message = connection.receive();
-                System.out.println("Received message: " + message);
-        }
+         String message = connection.receive();
+         connection.setTokenFromSuccessMessage(message);
+         System.out.println("Received message: " + message);
+      }
       connection.disconnect();
    }
 }
--- assign2/src/hangman/src/main/java/fe/up/pt/Server.java
+++ assign2/src/hangman/src/main/java/fe/up/pt/Server.java
@@ -10,151 +10,42 @@ import java.util.*;
 
 public class Server {
     private final int port;
-    private final int mode;
-    private Dictionary<String, List<String>> word_list;
+    private List<Game> games;
+    private Dictionary<String, List<String>> wordList;
+    private List<String> themes = new ArrayList<>();
     private List<Client> registeredUsers;
     private List<String> loggedUsers = new ArrayList<>();
     private ServerSocketChannel serverSocketChannel;
     private static final int maxPlayers = 5;
 
-    public Server(int port, int mode, String host){
-       this.port = port;
-       this.mode = mode;
-       this.word_list = readWordList();
-       this.registeredUsers = readUsers();
+    public Server(int port, String host) {
+        this.port = port;
+        this.wordList = readWordList();
+        this.registeredUsers = readUsers();
+        this.createGames(2,1);
     }
 
-    private Dictionary<String, List<String>> readWordList(){
-        Dictionary<String, List<String>> wordList = new Hashtable<>();
-        String line = "";
-
-        try (BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\fe\\up\\pt\\word_list.csv"))) {
-            boolean first = true;
-            while ((line = br.readLine()) != null) {
-                if (first){
-                    first = false;
-                    continue;
-                }
-                // use comma as separator
-                String[] data = line.split(",");
-                String theme = data[0];
-                String word = data[1];
-                List<String> words = wordList.get(theme);
-
-                if (words == null) {
-                    words = new ArrayList<>();
-                    wordList.put(theme, words);
-                }
-                else {
-                    words.add(word);
-                }
-
-            }
-        } catch (IOException ignored) {
+    private void createGames(int nTotalGames, int nRankedGames){
+        for (int i = 0; i < nTotalGames; i++){
+            Random rand = new Random();
+            String theme = this.themes.get(rand.nextInt(this.themes.size()));
+            String word = this.wordList.get(theme).get(rand.nextInt(this.wordList.get(theme).size()));
+            this.games.add(new Game(maxPlayers, new ArrayList<>(), i < nRankedGames, theme, word));
         }
 
-        return wordList;
     }
 
-    private List<Client> readUsers(){
-        List<Client> users = new ArrayList<>();
-        String line = "";
-
-        try (BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\fe\\up\\pt\\users.csv"))) {
-            boolean first = true;
-            while ((line = br.readLine()) != null) {
-                if (first){
-                    first = false;
-                    continue;
-                }
-                // use comma as separator
-                String[] data = line.split(",");
-                String username = data[0];
-                String password = data[1];
-                int rank = Integer.parseInt(data[2]);
-                users.add(new Client(username, password,"" ,rank));
-            }
-        } catch (IOException ignored) {
-        }
-
-        return users;
-    }
-
-    public void start() throws IOException {
-        this.serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.bind(new InetSocketAddress(this.port));
-        System.out.println("Server started on mode " + this.mode + " on port " + this.port);
-    }
-    private String readMessage(SocketChannel client) throws IOException{
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        client.read(buffer);
-        buffer.flip();
-        ByteBuffer validBuffer = ByteBuffer.allocate(buffer.remaining());
-        validBuffer.put(buffer);
-        validBuffer.flip();
-
-        return new String(validBuffer.array(), StandardCharsets.UTF_8);
-    }
-
-    private void sendMessage(SocketChannel client, String message) throws IOException{
-        client.write(ByteBuffer.wrap(message.getBytes()));
-    }
-
-    private Client loginUser(String username, String password) {
-        for (Client user: this.registeredUsers){
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)){
-                System.out.println("User " + username + " logged in!");
-                String token = user.getToken();
-
-                if (token.isEmpty()) user.setToken(UUID.randomUUID().toString());
-                if (!this.loggedUsers.contains(token)) this.loggedUsers.add(token);
-
-                return user;
-            }
-        }
-        return null;
-    }
-
-    private Client registerUser(String username, String password) {
-        for (Client user: this.registeredUsers){
-            if (user.getUsername().equals(username)){
-                return null;
-            }
-        }
-        String token = UUID.randomUUID().toString();
-        Client newUser = new Client(username, password, token,1000);
-        try {
-            // Open the file
-            FileWriter fileWriter = new FileWriter("src\\main\\java\\fe\\up\\pt\\users.csv", true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(newUser.getUsername() + "," + newUser.getPassword() + ","  + newUser.getRank() + "\n");
-            bufferedWriter.close();
-        } catch (IOException ignored) {
-            return null;
-        }
-        this.registeredUsers.add(newUser);
-        this.loggedUsers.add(token);
-
-        return newUser;
-    }
-
-    private boolean logoutUser(String token) {
-        for (String loggedToken: this.loggedUsers){
-            if (loggedToken.equals(token)){
-                this.loggedUsers.remove(token);
-                return true;
-            }
-        }
-        return false;
+    private void addPlayerToGame(String token, Game game){
+        game.addPlayer(token);
     }
 
     public static void main(String[] args) throws IOException {
-        Server server = new Server(12345, 1, "localhost");
+        Server server = new Server(8000, "localhost");
         server.start();
         boolean run = true;
 
         //TODO: seperate this loop on threads
-        while (run){
+        while (run) {
             SocketChannel client = server.serverSocketChannel.accept();
             while (run) {
                 String message = server.readMessage(client);
@@ -189,7 +80,7 @@ public class Server {
                         } else {
                             server.sendMessage(client, "ERR");
                         }
-                        
+
                         if (server.loggedUsers.isEmpty()) run = false;
                         System.out.println("Server shutting down!");
                         break;
@@ -201,5 +92,130 @@ public class Server {
             }
         }
         server.serverSocketChannel.close();
+    }
+
+    private Dictionary<String, List<String>> readWordList() {
+        Dictionary<String, List<String>> wordList = new Hashtable<>();
+        String line = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\fe\\up\\pt\\word_list.csv"))) {
+            boolean first = true;
+            while ((line = br.readLine()) != null) {
+                if (first) {
+                    first = false;
+                    continue;
+                }
+                // use comma as separator
+                String[] data = line.split(",");
+                String theme = data[0];
+                String word = data[1];
+                List<String> words = wordList.get(theme);
+
+                if (words == null) {
+                    this.themes.add(theme);
+                    words = new ArrayList<>();
+                    wordList.put(theme, words);
+                } else {
+                    words.add(word);
+                }
+
+            }
+        } catch (IOException ignored) {
+        }
+
+        return wordList;
+    }
+
+    public void start() throws IOException {
+        this.serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(this.port));
+        System.out.println("Server started on port " + this.port);
+    }
+
+    private List<Client> readUsers() {
+        List<Client> users = new ArrayList<>();
+        String line = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\fe\\up\\pt\\users.csv"))) {
+            boolean first = true;
+            while ((line = br.readLine()) != null) {
+                if (first) {
+                    first = false;
+                    continue;
+                }
+                // use comma as separator
+                String[] data = line.split(",");
+                String username = data[0];
+                String password = data[1];
+                int rank = Integer.parseInt(data[2]);
+                users.add(new Client(username, password, "", rank));
+            }
+        } catch (IOException ignored) {
+        }
+
+        return users;
+    }
+
+    private String readMessage(SocketChannel client) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        client.read(buffer);
+        buffer.flip();
+        ByteBuffer validBuffer = ByteBuffer.allocate(buffer.remaining());
+        validBuffer.put(buffer);
+        validBuffer.flip();
+
+        return new String(validBuffer.array(), StandardCharsets.UTF_8);
+    }
+
+    private void sendMessage(SocketChannel client, String message) throws IOException {
+        client.write(ByteBuffer.wrap(message.getBytes()));
+    }
+
+    private Client loginUser(String username, String password) {
+        for (Client user : this.registeredUsers) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                System.out.println("User " + username + " logged in!");
+                String token = user.getToken();
+
+                if (token.isEmpty()) user.setToken(UUID.randomUUID().toString());
+                if (!this.loggedUsers.contains(token)) this.loggedUsers.add(token);
+
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private Client registerUser(String username, String password) {
+        for (Client user : this.registeredUsers) {
+            if (user.getUsername().equals(username)) {
+                return null;
+            }
+        }
+        String token = UUID.randomUUID().toString();
+        Client newUser = new Client(username, password, token, 1000);
+        try {
+            // Open the file
+            FileWriter fileWriter = new FileWriter("src\\main\\java\\fe\\up\\pt\\users.csv", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(newUser.getUsername() + "," + newUser.getPassword() + "," + newUser.getRank() + "\n");
+            bufferedWriter.close();
+        } catch (IOException ignored) {
+            return null;
+        }
+        this.registeredUsers.add(newUser);
+        this.loggedUsers.add(token);
+
+        return newUser;
+    }
+
+    private boolean logoutUser(String token) {
+        for (String loggedToken : this.loggedUsers) {
+            if (loggedToken.equals(token)) {
+                this.loggedUsers.remove(token);
+                return true;
+            }
+        }
+        return false;
     }
 }
